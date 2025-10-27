9d35148fce4688888c2a4b2a266d9c27cd80d2f7
FujiiKohaku <k024g0078@g.neec.ac.jp>
2025-10-28 05:28:55 +0900

    最新　　　　　　

--- project/Player/Player.h
+++ project/Player/Player.h
@@ -93,7 +93,7 @@ public:
 
     void VelocityReset();
 
-    Pendulum* GetPendulum() const { return pendulum_; }
+    Pendulum* GetPendulum() const { return pendulum_; }　
 
     // ゲッターロボ
     const Vector3& GetPosition() const { return position_; }
